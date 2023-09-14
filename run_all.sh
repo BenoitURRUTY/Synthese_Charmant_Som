b5111e43787d467b0a0de157a10d12682c1c007d
Benoit Urruty <benoit.urruty@univ-grenoble-alpes.fr>
2023-09-14 14:45:08 +0200

    Synthèse CS

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,6 @@
 find * -iname "thconfig" | while read line; do
 	echo "Processing file '${line::-8}"
 	cd "${line::-8}"
-	therion thconfig
+	therion thconfig > ~/Documents/Spéléo/database_speleo/Chartreuse/Réseau_ded/topo_kriska/therion_kriska/test.txt
 	cd ~/Documents/Spéléo/database_speleo/Chartreuse/Réseau_ded/topo_kriska/therion_kriska
 done
