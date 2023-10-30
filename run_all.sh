a99ebe592162823d25271fb2c4e0ae3dbf7bc847
Benoit Urruty <benoit.urruty@univ-grenoble-alpes.fr>
2023-10-30 15:13:16 +0100

    caillou-bouge

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,6 @@
 find * -iname "thconfig" | while read line; do
 	echo "Processing file '${line::-8}"
 	cd "${line::-8}"
-	therion thconfig > ~/Documents/Spéléo/database_speleo/Chartreuse/Réseau_ded/topo_kriska/therion_kriska/test.txt
-	cd ~/Documents/Spéléo/database_speleo/Chartreuse/Réseau_ded/topo_kriska/therion_kriska
+	therion thconfig > ~/Documents/Speleo/database_speleo/Chartreuse/Reseau_ded/topo_kriska/therion_kriska/test.txt
+	cd ~/Documents/Speleo/database_speleo/Chartreuse/Reseau_ded/topo_kriska/therion_kriska
 done
